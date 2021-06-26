626fc7ac8d1d5ff98f2d90e864b435fa8bd51547
Виктория Земцева <jlgfmhldf@gmail.com>
2021-06-26 12:35:27 +0300

    yarn add plop for generation components

---
+++ plopfile.js
@@ -0,0 +1,21 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a reusable component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        // Plop will create directories for us if they do not exist
+        // so it's okay to add files in nested locations.
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'plop-templates/Component.js.hbs',
+      },
+    ],
+  })
+}
