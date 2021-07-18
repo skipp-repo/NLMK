9d3c77032a8bae49f9947a8a08977a06d1244788
Виктория Земцева <jlgfmhldf@gmail.com>
2021-07-18 21:11:02 +0300

    add static autocompliting

--- src/components/PopupSearch/PopupSearch.tsx
+++ src/components/PopupSearch/PopupSearch.tsx
@@ -1,18 +1,62 @@
 import React from 'react'
 import clsx from 'clsx'
+import Autosuggest from 'react-autosuggest'
 import './PopupSearch.scss'
 import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg'
 
 export type PopupSearchProps = JSX.IntrinsicElements['input'] & {}
 
-const PopupSearch: React.FC<PopupSearchProps> = ({ children, className, ...props }) => {
+const suggestions = [
+  {
+    text: 'Apple',
+  },
+  {
+    text: 'Banana',
+  },
+  {
+    text: 'Cherry',
+  },
+  {
+    text: 'Grapefruit',
+  },
+  {
+    text: 'Lemon',
+  },
+  {
+    text: 'Lemon2',
+  },
+  {
+    text: 'Lemon3',
+  },
+]
+
+const renderSuggestion = (suggestion) => suggestion.text
+
+const PopupSearch: React.FC<PopupSearchProps> = ({ children, className, onChange, ...props }) => {
+  const [value, setValue] = React.useState('')
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    setValue(value)
+    onChange(event)
+  }
+
   return (
     <div className={clsx('PopupSearch', className)}>
-      <input
-        type="text"
-        placeholder="Перевести слово или фразу..."
-        className="PopupSearch-input"
-        {...props}
+      <Autosuggest
+        suggestions={suggestions}
+        inputProps={{
+          // @ts-ignore
+          value,
+          placeholder: 'Перевести слово или фразу...',
+          className: 'PopupSearch-input',
+          onChange: handleChange,
+          ...props,
+        }}
+        renderSuggestion={renderSuggestion}
+        className="PopupSearch-suggestions"
       />
       <SearchIcon className="PopupSearch-icon" />
     </div>
