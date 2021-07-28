3c63f0c283af61b51b70d8299ee71ff1788674bb
Виктория Земцева <jlgfmhldf@gmail.com>
2021-07-28 16:50:13 +0300

    update sync chrome storage to local

--- src/redux/storage.ts
+++ src/redux/storage.ts
@@ -2,4 +2,5 @@ import { isDevServer } from '../constantes'
 import localStorage from 'redux-persist/lib/storage'
 import createChromeStorage from 'redux-persist-chrome-storage'
 
-export default isDevServer ? localStorage : createChromeStorage(window.chrome, 'sync')
+// использую local вместо sync из-за ограничений на вес данных в хранилище
+export default isDevServer ? localStorage : createChromeStorage(window.chrome, 'local')
