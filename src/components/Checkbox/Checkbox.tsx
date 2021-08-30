6749ff819992b6fa933dd9790c3929d7cf584e55
Виктория Земцева <jlgfmhldf@gmail.com>
2021-08-30 10:32:43 +0300

    update Checkbox component & TranlsationCardSelectable

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import clsx from 'clsx'
 import './Checkbox.scss'
+import { ReactComponent as Icon } from '../../assets/icons/checkbox-tick.svg'
 
-export type CheckboxProps = JSX.IntrinsicElements['div'] & {}
+export type CheckboxProps = JSX.IntrinsicElements['input'] & { disabled?: boolean }
 
-const Checkbox: React.FC<CheckboxProps> = ({ children, className, ...props }) => {
+const Checkbox: React.FC<CheckboxProps> = ({ children, disabled, className, ...props }) => {
   return (
-    <div {...props} className={clsx('Checkbox', className)}>
-      <input type="checkbox" className="Checkbox-input" />
-      <div className="Checkbox-icon"></div>
+    <div className={clsx('Checkbox', className, disabled && 'Checkbox_disabled')}>
+      <input type="checkbox" className="Checkbox-input" disabled={disabled} {...props} />
+      <div className="Checkbox-icon">
+        <Icon className="Checkbox-tick" />
+      </div>
     </div>
   )
 }
