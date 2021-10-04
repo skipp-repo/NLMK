0f1b57c7feeaab2984a0b0b2e88c2efd8154fa63
Виктория Земцева <jlgfmhldf@gmail.com>
2021-10-04 09:02:48 +0300

    add Glossaries page

--- src/containers/Glossaries/Glossaries.tsx
+++ src/containers/Glossaries/Glossaries.tsx
@@ -1,13 +1,237 @@
 import React from 'react'
+import { useAsyncCallback } from 'react-async-hook'
+import useTitle from 'react-use/lib/useTitle'
+import { useSelector } from 'react-redux'
+import { useDebouncedCallback } from 'use-debounce'
+import { downloadAllVocabs as downloadAllVocabsRequest } from '../../api/requests/downloadAllVocabs'
+import Container from '../../components/Container/Container'
+import PageTitle from '../../components/PageTitle/PageTitle'
+import DownloadLink from '../../components/DownloadLink/DownloadLink'
+import Tabs from '../../components/Tabs/Tabs'
+import TranslationCardSelectable from '../../components/TranslationCard/TranslationCardSelectable/TranslationCardSelectable'
+import './Glossaries.scss'
+import useReduxAction from '../../hooks/useReduxAction'
+import * as userSlice from '../../redux/slices/user'
+import * as vocabsSlice from '../../redux/slices/vocabs'
+import * as appSlice from '../../redux/slices/app'
+import GlossariesActions from './GlossariesActions'
+import Search from '../../components/Search/Search'
+import VocabsFilters from '../../components/VocabsFilters/VocabsFilters'
+import * as translationSlice from '../../redux/slices/translation'
+import { Translate } from '../../redux/slices/translation'
 
-export type GlossariesProps = {
+export type GlossariesProps = {}
 
+const title = 'Глоссарии НМЛК'
+
+const searchFiltersReducer = (state, action) => {
+  switch (action.type) {
+    case 'WordsAndPhrases':
+      return { ...state, words: action.selected, phrases: action.selected }
+    case 'Words': {
+      return { ...state, words: action.selected, phrases: false }
+    }
+    case 'Phrases':
+      return { ...state, words: false, phrases: action.selected }
+    case 'Glossary': {
+      let glossaries = state.glossaries
+
+      if (Array.isArray(glossaries)) {
+        if (action.selected) {
+          glossaries = [...glossaries, action.data.id]
+        } else {
+          glossaries = glossaries.filter((id) => id !== action.data.id)
+
+          if (!glossaries.length) {
+            glossaries = true
+          }
+        }
+      } else {
+        if (action.selected) {
+          glossaries = [action.data.id]
+        }
+      }
+
+      return { ...state, glossaries }
+    }
+    default:
+      return state
+  }
 }
 
-const Glossaries: React.FC<GlossariesProps> = ({ children }) => {
+const Glossaries: React.FC<GlossariesProps> = () => {
+  const reduxAction = useReduxAction()
+
+  const [query, setQuery] = React.useState('')
+  const [activeTab, setActiveTab] = React.useState()
+
+  const [searchFilters, searchFiltersDispatch] = React.useReducer(searchFiltersReducer, {
+    glossaries: true,
+    phrases: true,
+    words: true,
+    vocabs: true,
+  })
+
+  const vocabsByID = useSelector(vocabsSlice.selectors.vocabById(activeTab))
+  const vocabs = useSelector(vocabsSlice.selectors.vocabsList)
+  const token = useSelector(userSlice.selectors.token)
+  const glossaries = useSelector(userSlice.selectors.glossaries)
+
+  const selectCard = reduxAction(vocabsSlice.selectCard)
+  const getData = reduxAction(appSlice.getData)
+  const search = reduxAction((params: Omit<Translate, 'space'>) =>
+    translationSlice.translate({ space: 'MainVocabs', ...params }),
+  )
+  const debouncedSearch = useDebouncedCallback(search, 300)
+  const downloadAllVocabs = useAsyncCallback(async () => {
+    return await downloadAllVocabsRequest({ token })
+  })
+
+  const searchData = useSelector(translationSlice.selectors.mainVocabsSearchResults)
+
+  const needShowSearchResults = !!searchData?.results?.length && query.length > 0
+
+  const glossariesFilterList = React.useMemo(
+    () =>
+      glossaries.map(({ _id, name, ...item }) => {
+        let selected
+
+        if (Array.isArray(searchFilters.glossaries)) {
+          selected = searchFilters.glossaries.includes(_id)
+        } else {
+          selected = false
+        }
+
+        return {
+          type: 'Glossary',
+          key: `Glossary-${_id}`,
+          data: { id: _id },
+          name: name,
+          selected,
+        }
+      }),
+    [glossaries, searchFilters.glossaries],
+  )
+
+  const filtersData = [
+    {
+      name: 'Тип поиска',
+      items: [
+        {
+          type: 'WordsAndPhrases',
+          key: 'WordsAndPhrases',
+          name: 'Слова и фразы',
+          selected: searchFilters.phrases && searchFilters.words,
+        },
+        {
+          type: 'Words',
+          key: 'Words',
+          name: 'Только слова',
+
+          selected: searchFilters.words && !searchFilters.phrases,
+        },
+        {
+          type: 'Phrases',
+          key: 'Phrases',
+          name: 'Только фразы',
+          selected: searchFilters.phrases && !searchFilters.words,
+        },
+      ],
+    },
+    {
+      name: 'Глоссарий',
+      items: glossariesFilterList,
+    },
+  ]
+
+  const tabs = vocabs.map(({ _id, name }) => ({
+    name: name || 'default',
+    id: _id,
+  }))
+
+  const handleCardSelect = (cardId: number, selected: boolean) => {
+    selectCard({ vocabId: activeTab, cardId, selected })
+  }
+
+  const renderCard = (data) => {
+    return (
+      <TranslationCardSelectable
+        key={data._id}
+        className={'Glossaries-card'}
+        item={data}
+        speech
+        onSpeech={handleSpeech}
+        onSelect={handleCardSelect}
+      />
+    )
+  }
+
+  const handleSpeech = () => {}
+
+  const handleTabChange = (id) => setActiveTab(id)
+
+  const handleSearch = (text) => {
+    setQuery(text)
+  }
+
+  const handleChangeFilter = ({ type, selected, data }) => {
+    searchFiltersDispatch({ type, selected, data })
+  }
+
+  useTitle(title)
+
+  React.useEffect(() => {
+    getData()
+  }, [])
+
+  React.useEffect(() => {
+    if (vocabs?.length && !activeTab) {
+      setActiveTab(vocabs[0]._id)
+    }
+  }, [vocabs])
+
+  React.useEffect(() => {
+    if (query) {
+      debouncedSearch({
+        query,
+        filters: {
+          common: false,
+          ...searchFilters,
+        },
+      })
+    }
+  }, [searchFilters, query])
+
   return (
-    <div>
-      Glossaries
+    <div className="Glossaries">
+      <Container className="Glossaries-header">
+        <PageTitle className="Glossaries-title">{title}</PageTitle>
+
+        <DownloadLink onClick={downloadAllVocabs.execute}>Скачать все слова</DownloadLink>
+      </Container>
+      <Container className="Glossaries-tabs">
+        <Tabs tabs={tabs} onChange={handleTabChange} />
+      </Container>
+
+      {vocabsByID?.cards?.length !== undefined && <GlossariesActions activeTab={activeTab} />}
+
+      <Container className="Glossaries-search">
+        <Search className="Glossaries-search-input" onChange={handleSearch} suggestions={[]} />
+
+        <VocabsFilters
+          className="Glossaries-search-filter"
+          items={filtersData}
+          onSelect={handleChangeFilter}
+        />
+      </Container>
+
+      {!!vocabsByID?.cards?.length && !needShowSearchResults && (
+        <Container className="Glossaries-cards">{vocabsByID.cards.map(renderCard)}</Container>
+      )}
+
+      {needShowSearchResults && (
+        <Container className="Glossaries-cards">{searchData.results.map(renderCard)}</Container>
+      )}
     </div>
   )
 }
---
+++ src/containers/Glossaries/GlossariesActions.tsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import proschet from 'proschet'
+import { useAsyncCallback } from 'react-async-hook'
+import { downloadVocabById } from '../../api/requests/downloadVocabById'
+import ItemsCount from '../../components/ItemsCount/ItemsCount'
+import Checkbox from '../../components/Checkbox/Checkbox'
+import Container from '../../components/Container/Container'
+import IconButton from '../../components/IconButton/IconButton'
+import { useSelector } from 'react-redux'
+import * as userSlice from '../../redux/slices/user'
+import * as vocabsSlices from '../../redux/slices/vocabs'
+import { ReactComponent as DownloadIcon } from '../../assets/icons/download.svg'
+import VocabsDropdown from '../../components/VocabsDropdown/VocabsDropdown'
+import * as vocabsSlice from '../../redux/slices/vocabs'
+import useReduxAction from '../../hooks/useReduxAction'
+
+export type GlossariesProps = JSX.IntrinsicElements['div'] & { activeTab: string }
+
+const words = proschet(['слово', 'слова', 'слов'])
+
+const Glossaries: React.FC<GlossariesProps> = ({ children, className, activeTab, ...props }) => {
+  const reduxAction = useReduxAction()
+  const vocabsByID = useSelector(vocabsSlices.selectors.vocabById(activeTab))
+
+  const vocabs = useSelector(vocabsSlice.selectors.vocabsList)
+
+  const selectedIds = useSelector(vocabsSlice.selectors.selectedCardsIdsByVocabId(activeTab))
+
+  const [checkAll, setCheckAll] = React.useState<undefined | boolean>()
+  const [vocabIdsForMoving, setVocabsIdsForMoving] = React.useState([])
+
+  const editFolder = reduxAction(vocabsSlices.editFolder)
+  const selectAll = reduxAction(vocabsSlice.selectAll)
+
+  const token = useSelector(userSlice.selectors.token)
+
+  const downloadCurrentVocab = useAsyncCallback(async (id) => {
+    return await downloadVocabById({ token, id })
+  })
+
+  const fixedVocabs = React.useMemo(
+    () =>
+      vocabs
+        .filter(({ _id }) => activeTab !== _id)
+        .map(({ _id, name, ...item }) => {
+          return {
+            id: _id,
+            name: item.default ? 'Default' : name,
+            checked: vocabIdsForMoving.includes(_id),
+          }
+        }),
+    [vocabs, activeTab, vocabIdsForMoving],
+  )
+
+  const handleSelectAll = ({ target }) => {
+    setCheckAll(target.checked ? true : undefined)
+    selectAll({ vocabId: activeTab, select: target.checked })
+  }
+
+  const handleUnselectAll = ({ target: { checked } }) => {
+    setCheckAll(checked ? false : undefined)
+    if (checked) {
+      selectAll({ vocabId: activeTab, select: false })
+    }
+  }
+
+  const handleSelectGroupForMoving = (id, checked) => {
+    if (checked) {
+      setVocabsIdsForMoving((state) => [...state, id])
+
+      editFolder({ id: activeTab, cardsToRemove: selectedIds })
+      editFolder({ id, cardsToAdd: selectedIds })
+
+      setTimeout(() => {
+        setVocabsIdsForMoving([])
+      }, 500)
+    } else {
+      setVocabsIdsForMoving((state) => {
+        return state.filter((stateId) => stateId !== id)
+      })
+    }
+  }
+
+  const handleDownloadVocab = () => {
+    downloadCurrentVocab.execute(activeTab)
+  }
+
+  React.useEffect(() => {
+    setCheckAll(undefined)
+  }, [activeTab])
+
+  return (
+    <Container className="Glossaries-actions">
+      <ItemsCount className="Glossaries-actions-count">{`${vocabsByID?.cards.length} ${words(
+        vocabsByID?.cards.length,
+      )}`}</ItemsCount>
+
+      <VocabsDropdown
+        text="Переместить в группу"
+        items={fixedVocabs}
+        onSelect={handleSelectGroupForMoving}
+        disabled={!selectedIds.length}
+      />
+      <Checkbox
+        className="Glossaries-actions-button"
+        text="Отметить все"
+        secondary
+        checked={checkAll}
+        onChange={handleSelectAll}
+      />
+      <Checkbox
+        className="Glossaries-actions-button"
+        text="Снять все"
+        secondary
+        checked={checkAll === false}
+        onChange={handleUnselectAll}
+      />
+      <IconButton
+        className="Glossaries-actions-button"
+        text="Скачать"
+        Icon={DownloadIcon}
+        onClick={handleDownloadVocab}
+      />
+    </Container>
+  )
+}
+
+export default React.memo(Glossaries)
