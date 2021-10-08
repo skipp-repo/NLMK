133b1df2b02717babcadccdc4422551a8a636c13
Виктория Земцева <jlgfmhldf@gmail.com>
2021-10-08 09:38:48 +0300

    change logic for generating user token

--- src/utils/getUserToken.ts
+++ src/utils/getUserToken.ts
@@ -1,12 +1,5 @@
-import chromep from 'chrome-promise'
 import { nanoid } from 'nanoid'
 
 export default async (): Promise<string> => {
-  if (chromep.identity) {
-    const data = await chromep.identity.getProfileUserInfo()
-
-    return data.email
-  } else {
-    return `${nanoid(20)}-${Date.now()}`
-  }
+  return `${nanoid(20)}-${Date.now()}`
 }
