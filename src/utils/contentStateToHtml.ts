d6210d5572682bc0963b291fb5d65fdee5e184bc
Виктория Земцева <jlgfmhldf@gmail.com>
2021-11-24 21:53:47 +0300

    fix wrapping html

--- src/utils/contentStateToHtml.ts
+++ src/utils/contentStateToHtml.ts
@@ -4,7 +4,5 @@ import { stateToHTML } from 'draft-js-export-html'
 export default (content: ContentState): string => {
   const html = stateToHTML(content)
 
-  const fullHtml = `<!DOCTYPE <html><head></head></html><html><body>${html}</body></html>`
-
-  return fullHtml
+  return `<!DOCTYPE html><html lang='ru'><head><title></title></head><body>${html}</body></html>`
 }
