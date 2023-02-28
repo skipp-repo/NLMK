0dad0df0ff628cad92ee5df8c11c15bdbeace74f
Земцева Виктория <zemtseva@msk.atb.su>
2023-02-28 14:48:41 +0300

    Revert "remove uploading documents"
    This reverts commit b2bb29aa627d5cde4dd91e094bf0112005c35271.

--- src/containers/Documents/Documents.tsx
+++ src/containers/Documents/Documents.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { useDebouncedCallback } from 'use-debounce'
 import { useLocation } from 'wouter'
 import { downloadDocumentById as downloadDocumentByIdRequest } from '../../api/requests/downloadDocumentById'
+import Button from '../../components/Button/Button'
 import Container from '../../components/Container/Container'
 import PageTitle from '../../components/PageTitle/PageTitle'
 import './Documents.scss'
@@ -25,6 +26,7 @@ const Documents: React.FC<MyDocumentsProps> = () => {
 
   const documents = useSelector(documentsSlice.selectors.documentsList)
 
+  const uploadDocument = reduxAction(documentsSlice.uploadDocument)
   const getDocuments = reduxAction(documentsSlice.getDocuments)
   const selectItem = reduxAction(documentsSlice.selectItem)
   const deleteDocument = reduxAction(documentsSlice.deleteDocument)
@@ -34,6 +36,12 @@ const Documents: React.FC<MyDocumentsProps> = () => {
   const downloadDocumentById = useAsyncCallback(async (id) => {
     return await downloadDocumentByIdRequest({ id })
   })
+
+  const handleFileInputChange = ({ target }) => {
+    const file = target.files[0]
+
+    uploadDocument({ file })
+  }
 
   const handleNewDocument = () => {
     setLocation('/documents/edit/new')
@@ -78,6 +86,18 @@ const Documents: React.FC<MyDocumentsProps> = () => {
     <div className="Documents">
       <Container className="Documents-header">
         <PageTitle className="Documents-title">{title}</PageTitle>
+
+        <form className="Documents-button">
+          <input
+            type="file"
+            name="userDoc"
+            required
+            className="Documents-input-file"
+            accept=".doc,.docx"
+            onChange={handleFileInputChange}
+          />
+          <Button className="Documents-button">ЗАГРУЗИТЬ ДОКУМЕНТ</Button>
+        </form>
       </Container>
 
       <DocumentsActions />
